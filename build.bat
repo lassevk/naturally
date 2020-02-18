10ed642cc25db9bc61c0bd841adae15bc1b35da2
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2020-02-18 12:24:26 +0100

    Add signing of assembly

--- build.bat
+++ build.bat
@@ -5,6 +5,9 @@ setlocal
 if exist *.nupkg del *.nupkg
 if errorlevel 1 goto error
 
+if exist *.snupkg del *.snupkg
+if errorlevel 1 goto error
+
 for /d %%f in (*.*) do (
     if exist "%%f\bin" rd /s /q "%%f\bin"
     if errorlevel 1 goto error
@@ -13,19 +16,31 @@ for /d %%f in (*.*) do (
 )
 if errorlevel 1 goto error
 
+set SUFFIX=
+for /f %%f in ('git rev-parse --abbrev-ref HEAD') do set GITBRANCH=%%f
+
+if not "%GITBRANCH%" == "master" (
+    set SUFFIX=beta
+)
+
+
 dotnet restore
 if errorlevel 1 goto error
 
-dotnet build
+dotnet build /p:VERSIONSUFFIX=%SUFFIX%
 if errorlevel 1 goto error
 
 dotnet test
 if errorlevel 1 goto error
 
 for /d %%f in (*.*) do (
-    if exist "%%f\bin\Debug\*.nupkg" copy "%%f\bin\Debug\*.nupkg" .\
+    if exist "%%f\bin\Debug\*.nupkg" move "%%f\bin\Debug\*.nupkg" .\
     if errorlevel 1 goto error
-    if exist "%%f\bin\Release\*.nupkg" copy "%%f\bin\Release\*.nupkg" .\
+    if exist "%%f\bin\Debug\*.snupkg" move "%%f\bin\Debug\*.snupkg" .\
+    if errorlevel 1 goto error
+    if exist "%%f\bin\Release\*.nupkg" move "%%f\bin\Release\*.nupkg" .\
+    if errorlevel 1 goto error
+    if exist "%%f\bin\Release\*.snupkg" move "%%f\bin\Release\*.snupkg" .\
     if errorlevel 1 goto error
 )
 
